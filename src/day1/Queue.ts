5afd3da3aef776fd9c4dc2c1eeddbeb380cf1ac8
Leonidas Antoniou <swoldev@proton.me>
2024-10-16 11:54:14 +0100

    Stack & Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,46 @@
+type Node<T> = {
+  value: T,
+  next?: Node<T>
+}
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    const node = { value: item } as Node<T>;
+    this.length++;
+    if (!this.tail || !this.head) {
+      this.tail = this.head = node;
+      return;
     }
 
-    enqueue(item: T): void {
+    this.tail.next = node;
+    this.tail = node;
+  }
 
-}
-    deque(): T | undefined {
+  deque(): T | undefined {
+    if (!this.head) {
+      return undefined;
+    }
 
-}
-    peek(): T | undefined {
+    this.length--;
 
-}
+    const head = this.head;
+    this.head = this.head.next;
+
+    head.next = undefined;
+
+    return head.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
